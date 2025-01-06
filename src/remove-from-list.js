87e4c1a93fbbd831951dbeacb7232f14b5b918b9
Anna Tochk <anitochka1105@gmail.com>
2025-01-07 03:51:35 +0500

    feat: add solution

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -22,11 +22,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+class ListNode {
+	constructor(x) {
+		this.value = x;  // Значение узла
+		this.next = null; // Ссылка на следующий узел
+	}
+}
+
+function removeKFromList(l, k) {
+	// Удаляем все узлы, начиная с головы, которые равны k
+	while (l !== null && l.value === k) {
+		// Перемещаем голову на следующий элемент
+		l = l.next;
+	}
+	// Начинаем обход с головы
+	let current = l;
+
+	// Обрабатываем остальные элементы списка
+	while (current !== null && current.next !== null) {
+		if (current.next.value === k) {
+			// Пропускаем узел со значением k
+			current.next = current.next.next;
+		} else {
+			// Переходим к следующему узлу
+			current = current.next;
+		}
+	}
+	// Возвращаем новый список
+	return l;
 }
 
 module.exports = {
-  removeKFromList
+	removeKFromList
 };
